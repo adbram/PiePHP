d1906d69bf46814203596c581369dcc4821f28d1
adzer <adnane.berramou@epitech.eu>
2020-03-29 00:26:37 +0100

    model (crud) OK, go request

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -8,7 +8,7 @@ class UserModel
     private $_password;
     private static $_db;
 
-    public function __construct($data)
+    public function __construct($data = [])
     {
         self::$_db = new  PDO('mysql:host=127.0.0.1;dbname=piephp;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         $this->hydrate($data);
@@ -28,7 +28,7 @@ class UserModel
     public function setEmail($email)
     {
         $email = htmlspecialchars($email);
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($email) <= 30) {
             $this->_email = $email;
             echo 'email [ OK ]<br>';
         } else {
@@ -56,17 +56,6 @@ class UserModel
         return $this->_password;
     }
 
-    public function save()
-    {
-        if($this->getEmail() != NULL && $this->getPassword() != NULL) {
-            $req = self::$_db->prepare('INSERT INTO users (email, password) VALUES (:email, :password);');
-            $req->execute(['email' => $this->getEmail(), 'password' => $this->getPassword()]);
-            echo 'save [ OK ]<br>';
-        } else {
-            echo 'save [ KO ]<br>';
-        }
-    }
-
     public function login()
     {
         if($this->getEmail() != NULL && $this->getPassword() != NULL) {
@@ -85,5 +74,45 @@ class UserModel
     {
         $_SESSION = [];
         session_destroy();
+    }
+
+    public function save() // alias create
+    {
+        if($this->getEmail() != NULL && $this->getPassword() != NULL) {
+            $req = self::$_db->prepare('INSERT INTO users (email, password) VALUES (:email, :password);');
+            $req->execute(['email' => $this->getEmail(), 'password' => $this->getPassword()]);
+            echo 'save [ OK ]<br>';
+        } else {
+            echo 'save [ KO ]<br>';
+        }
+    }
+
+    public function read($id)
+    {
+        $req = self::$_db->prepare('SELECT * FROM users WHERE id = :id ;');
+        $req->execute(['id' => $id]);
+        return $req->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($id)
+    {
+        $req = self::$_db->prepare('UPDATE users SET email = :email, password = :password;');
+        $req->execute(['email' => $this->getEmail(), 'password' => $this->getPassword()]);
+    }
+
+    public function delete($id)
+    {
+        $req = self::$_db->prepare('DELETE FROM users WHERE id = :id ;');
+        $req->execute(['id' => $id]);
+    }
+
+    public function readAll()
+    {
+        $toReturn = [];
+        $req = self::$_db->query('SELECT * FROM users;');
+        while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            $toReturn['user-'.$data['id']] = $data;
+        }
+        return $toReturn;
     }
 }
